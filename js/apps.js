b214ba2814a9d8a37d4babd6fde8cb352a04dbd2
IncrediDexer <157266806+IncrediDexer@users.noreply.github.com>
2025-04-15 17:30:24 -0400

    Update apps.js

--- js/apps.js
+++ js/apps.js
@@ -1,152 +1,206 @@
 versions.v1 = {
-  name: "Template",
-  version: "temp01",
-  date: "2025",
-  folder: "asset-v1/", // ← If you are adding more versions, make sure follder is correct, for example "asset-v2/" for v2
-  looptime: 5333,
-  bpm: 90,
-  totalframe: 260,
-  nbpolo: 7,
-  bonusloopA: false, // ← It makes bonus start on loop A, if its false, bonus will start after 1 loop
-  bonusendloopA: true,
-  colBck: "#0F0F0F",
-  col0: "#919191",
-  col1: "#5f5f5f",
-  col2: "#4b4b4b",
-  col3: "#373737",
-  col4: "#232323",
-  animearray: [
-    {
-      name: "temp_polo",
-      color: "7D7D7D",
-      uniqsnd: !0, // ← !0 means that polo will use only _a audio, if its !1 - it will use _a and _b
-    },
-    {
-      name: "temp_polo",
-      color: "7D7D7D",
-      uniqsnd: !0,
-    },
-    {
-      name: "temp_polo",
-      color: "7D7D7D",
-      uniqsnd: !0,
-    },
-    {
-      name: "temp_polo",
-      color: "7D7D7D",
-      uniqsnd: !0,
-    },
-    {
-      name: "temp_polo",
-      color: "7D7D7D",
-      uniqsnd: !0,
-    },
-    {
-      name: "temp_polo",
-      color: "7D7D7D",
-      uniqsnd: !0,
-    },
-    {
-      name: "temp_polo",
-      color: "7D7D7D",
-      uniqsnd: !0,
-    },
-    {
-      name: "temp_polo",
-      color: "7D7D7D",
-      uniqsnd: !0,
-    },
-    {
-      name: "temp_polo",
-      color: "7D7D7D",
-      uniqsnd: !0,
-    },
-    {
-      name: "temp_polo",
-      color: "7D7D7D",
-      uniqsnd: !0,
-    },
-    {
-      name: "temp_polo",
-      color: "7D7D7D",
-      uniqsnd: !0,
-    },
-    {
-      name: "temp_polo",
-      color: "7D7D7D",
-      uniqsnd: !0,
-    },
-    {
-      name: "temp_polo",
-      color: "7D7D7D",
-      uniqsnd: !0,
-    },
-    {
-      name: "temp_polo",
-      color: "7D7D7D",
-      uniqsnd: !0,
-    },
-    {
-      name: "temp_polo",
-      color: "7D7D7D",
-      uniqsnd: !0,
-    },
-    {
-      name: "temp_polo",
-      color: "7D7D7D",
-      uniqsnd: !0,
-    },
-    {
-      name: "temp_polo",
-      color: "7D7D7D",
-      uniqsnd: !0,
-    },
-    {
-      name: "temp_polo",
-      color: "7D7D7D",
-      uniqsnd: !0,
-    },
-    {
-      name: "temp_polo",
-      color: "7D7D7D",
-      uniqsnd: !0,
-    },
-    {
-      name: "temp_polo",
-      color: "7D7D7D",
-      uniqsnd: !0,
-    },
-  ],
-  bonusarray: [
-    {
-      name: "blank",
-      src: "b1-v1-blank-hb",
-      code: "1,2,3,4,5",
-      sound: "aspire-blank",
-      aspire: "aspire-blank",
-      loop: 1, // ← Every bonus have its own loops in this version
-    },
-  ],
+	name: "Aria Math",
+	version: "1",
+	date: "2025",
+	folder: "asset-v1/",
+	looptime: 5700,
+	bpm: 168,
+	totalframe: 274,
+	nbpolo: 7,
+	bonusloopA: false,
+	bonusendloopA: true,
+	colBck: "#0F0F0F",
+	col0: "#38103F",
+	col1: "#FFC039",
+	col2: "#FFC039",
+	col3: "#FFC039",
+	col4: "#143434",
+	animearray: [{
+		name: "1_Bush",
+		color: "FFC039",
+		uniqsnd: !0
+	}, {
+		name: "2_Ore",
+		color: "FFC039",
+		uniqsnd: !1
+	}, {
+		name: "3_Fruit",
+		color: "FFC039",
+		uniqsnd: !0
+	}, {
+		name: "4_Tree",
+		color: "FFC039",
+		uniqsnd: !1
+	}, {
+		name: "5_Ecosystem",
+		color: "FFC039",
+		uniqsnd: !1
+	}, {
+		name: "6_Coral",
+		color: "FFC039",
+		uniqsnd: !1
+	}, {
+		name: "7_Mines",
+		color: "FFC039",
+		uniqsnd: !0
+	}, {
+		name: "8_Jellyfish",
+		color: "FFC039",
+		uniqsnd: !1
+	}, {
+		name: "9_Submarine",
+		color: "FFC039",
+		uniqsnd: !1
+	}, {
+		name: "10_Abyssal",
+		color: "FFC039",
+		uniqsnd: !1
+	}, {
+		name: "11_Melodeem",
+		color: "FFC039",
+		uniqsnd: !1
+	}, {
+		name: "12_Musician",
+		color: "FFC039",
+		uniqsnd: !1
+	}, {
+		name: "13_Wandering",
+		color: "FFC039",
+		uniqsnd: !1
+	}, {
+		name: "14_Villager",
+		color: "FFC039",
+		uniqsnd: !1
+	}, {
+		name: "15_Gramophone",
+		color: "FFC039",
+		uniqsnd: !1
+	}, {
+		name: "16_Leader",
+		color: "FFC039",
+		uniqsnd: !1
+	}, {
+		name: "17_Reanimator",
+		color: "FFC039",
+		uniqsnd: !1
+	}, {
+		name: "18_Provider",
+		color: "FFC039",
+		uniqsnd: !1
+	}, {
+		name: "19_Mage",
+		color: "FFC039",
+		uniqsnd: !1
+	}, {
+		name: "20_Seer",
+		color: "FFC039",
+		uniqsnd: !1
+	}, {
+		name: "21_Jschlatt",
+		color: "FFC039",
+		uniqsnd: !1
+	}],
+	bonusarray: [],
 };
 
+// Another minecraft theme mod?!!?!
 versions.v2 = {
-  name: "Template",
-  version: "temp01",
-  date: "2025",
-  folder: "asset-v2/",
-  looptime: 10434,
-  bpm: 92,
-  totalframe: 500,
-  nbpolo: 7,
-  bonusloopA: false,
-  bonusendloopA: true,
-  colBck: "#0F0F0F",
-  col0: "#919191",
-  col1: "#5f5f5f",
-  col2: "#4b4b4b",
-  col3: "#373737",
-  col4: "#232323",
-  animearray: [
-{name:"1_Exolotl",color:"4baa62",uniqsnd:!0},{name:"2_Azalea Bush",color:"4baa62",uniqsnd:!0},{name:"3_Drip leaves",color:"4baa62",uniqsnd:!0},{name:"4_Lush Vines",color:"4baa62",uniqsnd:!0},{name:"5_Glare",color:"4baa62",uniqsnd:!1},{name:"6_Raw Copper",color:"e45444",uniqsnd:!0},{name:"7_Dripstone",color:"e45444",uniqsnd:!0},{name:"8_Copper Golem",color:"e45444",uniqsnd:!0},{name:"9_Dripstone Brass",color:"e45444",uniqsnd:!1},{name:"10_Caves Ambience",color:"e45444",uniqsnd:!0},{name:"11_Geode Guardian",color:"af81fb",uniqsnd:!0},{name:"12_Crystal Notes",color:"af81fb",uniqsnd:!1},{name:"13_Amethyst Collector",color:"af81fb",uniqsnd:!1},{name:"14_Enderman",color:"af81fb",uniqsnd:!1},{name:"15_Amethyst Statue",color:"af81fb",uniqsnd:!0},{name:"16_Warden",color:"53e3cb",uniqsnd:!1},{name:"17_Survivor",color:"53e3cb",uniqsnd:!1},{name:"18_Soulfire",color:"53e3cb",uniqsnd:!1},{name:"19_Elderly Allay",color:"53e3cb",uniqsnd:!1},{name:"20_Sweden",color:"53e3cb",uniqsnd:!1},],
-  bonusarray: [],
+	name: "Otherside",
+	version: "2",
+	date: "2024",
+	folder: "asset-v2/",
+	looptime: 10434,
+	bpm: 92,
+	totalframe: 500,
+	nbpolo: 7,
+	bonusloopA: false,
+	bonusendloopA: true,
+	colBck: "#0F0F0F",
+	col0: "#b1e283",
+	col1: "#4baa63",
+	col2: "#34794c",
+	col3: "#154c3c",
+	col4: "#143434",
+	animearray: [{
+		name: "1_Exolotl",
+		color: "4baa62",
+		uniqsnd: !0
+	}, {
+		name: "2_Azalea Bush",
+		color: "4baa62",
+		uniqsnd: !0
+	}, {
+		name: "3_Drip leaves",
+		color: "4baa62",
+		uniqsnd: !0
+	}, {
+		name: "4_Lush Vines",
+		color: "4baa62",
+		uniqsnd: !0
+	}, {
+		name: "5_Glare",
+		color: "4baa62",
+		uniqsnd: !1
+	}, {
+		name: "6_Raw Copper",
+		color: "e45444",
+		uniqsnd: !0
+	}, {
+		name: "7_Dripstone",
+		color: "e45444",
+		uniqsnd: !0
+	}, {
+		name: "8_Copper Golem",
+		color: "e45444",
+		uniqsnd: !0
+	}, {
+		name: "9_Dripstone Brass",
+		color: "e45444",
+		uniqsnd: !1
+	}, {
+		name: "10_Caves Ambience",
+		color: "e45444",
+		uniqsnd: !0
+	}, {
+		name: "11_Geode Guardian",
+		color: "af81fb",
+		uniqsnd: !0
+	}, {
+		name: "12_Crystal Notes",
+		color: "af81fb",
+		uniqsnd: !1
+	}, {
+		name: "13_Amethyst Collector",
+		color: "af81fb",
+		uniqsnd: !1
+	}, {
+		name: "14_Enderman",
+		color: "af81fb",
+		uniqsnd: !1
+	}, {
+		name: "15_Amethyst Statue",
+		color: "af81fb",
+		uniqsnd: !0
+	}, {
+		name: "16_Warden",
+		color: "53e3cb",
+		uniqsnd: !1
+	}, {
+		name: "17_Survivor",
+		color: "53e3cb",
+		uniqsnd: !1
+	}, {
+		name: "18_Soulfire",
+		color: "53e3cb",
+		uniqsnd: !1
+	}, {
+		name: "19_Elderly Allay",
+		color: "53e3cb",
+		uniqsnd: !1
+	}, {
+		name: "20_Sweden",
+		color: "53e3cb",
+		uniqsnd: !1
+	}, ],
+	bonusarray: [],
 };
